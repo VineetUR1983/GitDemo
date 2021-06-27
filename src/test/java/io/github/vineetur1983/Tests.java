748cafdd8f412b58a106ea71e03a09ef8f033c85
Vineet Reddy <vineetur@gmail.com>
2021-06-27 15:06:49 +1000

    Added New File into Master

---
+++ src/test/java/io/github/vineetur1983/Tests.java
@@ -0,0 +1,19 @@
+package io.github.vineetur1983;
+
+import org.testng.annotations.Test;
+
+public class Tests {
+	
+	@Test
+	public void LoginTest()
+	{
+		System.out.println("Fisrt Test for Login");
+	}
+	
+	@Test
+	public void VerifyTest()
+	{
+		System.out.println("Verify Successful Login");
+	}
+
+}
